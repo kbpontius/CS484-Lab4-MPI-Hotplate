70b807bcd367b50a2341aa169b4e1f2a616f988f
Kyle Pontius <kbpontius@gmail.com>
2015-12-15 23:59:32 -0700

    Initial Commit

---
+++ Lab 4 - MPI Hotplate/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab 4 - MPI Hotplate
+//
+//  Created by Kyle on 12/15/15.
+//  Copyright © 2015 Kyle Pontius. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
